0176e4f37b0bfec6a759515ffe64a8b1541f8c49
Photon <sergoqu@gmail.com>
2022-09-28 20:03:46 +0200

    add jest and eslint for scss files

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,32 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with first params', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with addictional class', () => {
+        const expected = 'someClass clases cls';
+        expect(classNames('someClass', {}, ['clases', 'cls'])).toBe(expected);
+    });
+    test('with addictional params', () => {
+        const expected = 'someClass clases cls hovered focus';
+        expect(classNames(
+            'someClass',
+            {
+                hovered: true,
+                scrolable: false,
+                focus: true,
+            },
+            ['clases', 'cls'],
+        ))
+            .toBe(expected);
+    });
+    test('with addictional params undefiend', () => {
+        const expected = 'someClass clases cls scrolable';
+        expect(classNames('someClass', {
+            hovered: undefined,
+            scrolable: true,
+        }, ['clases', 'cls']))
+            .toBe(expected);
+    });
+});
